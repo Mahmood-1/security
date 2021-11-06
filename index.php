7cccac61e2b1db3940f69cb36c339385cc80e058
Mahmood-1 <hooda2004m@gmail.com>
2021-11-06 21:00:31 +0200

    new url

--- index.php
+++ index.php
@@ -9,10 +9,32 @@
 </head>
 
 <body>
-    <a href="https://security1mahmood.herokuapp.com/results.php">results</a>
 </body>
 <script>
     fetch('https://security1mahmood.herokuapp.com/tracker.php')
 </script>
+
+
+<div class="jumbotron">
+    <div class="col-12 text-center">
+        <h3 class="heading">MAHMOOD SHAHEEN</h3>
+        <div class="heading-underline"></div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="img/computers.png">
+                </div>
+                <div class="col-md-8">
+                    <blockquote><i class="fas fa-quote-left"></i>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores quos numquam amet tenetur culpa magni odit laudantium obcaecati, soluta ab dolore quidem praesentium non nisi exercitationem delectus
+                        ut accusamus minima!</blockquote>
+                    <hr class="clients-hr">
+                    <cite>&#8212; Eric, Small Business Owner</cite>
+                </div>
+            </div>
+        </div>
+        <a href="https://security1mahmood.herokuapp.com/results.php">results</a>
 
 </html>
